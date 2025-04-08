318d8dd1755f6b9df7a289cd137fb656d4f0ab82
yeni023 <yeeun3011@naver.com>
2025-04-08 16:06:25 +0900

    feat: MainPage 라우팅 연결

---
+++ money_log/src/pages/MainPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="main-page">
+    <h1>Main Page 라우팅 연결 완료!</h1>
+  </div>
+</template>
+
+<script setup>
+console.log("MainPage.vue 라우팅 정상 연결됨!");
+</script>
+
+<style scoped>
+.main-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-size: 24px;
+  color: #2e7d32;
+  font-weight: bold;
+}
+</style>

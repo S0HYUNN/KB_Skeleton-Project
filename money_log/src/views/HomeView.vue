e6f22fda406acedf1dee796066bfb54733f0e4a1
SoHyun Park <pshsh0516@naver.com>
2025-04-08 15:25:07 +0900

    Feat: "router 서버 연결"

--- money_log/src/views/HomeView.vue
+++ money_log/src/views/HomeView.vue
@@ -1,9 +1,24 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const moneyList = ref([]);
+
+onMounted(async () => {
+  try {
+    const res = await axios.get('http://localhost:3000/money');
+    // const res = await axios.get('http://localhost:3000/money?date_gte=2025-04-01&date_lte=2025-04-03');
+    moneyList.value = res.data;
+    console.log('💰 가져온 가계부 데이터:', moneyList.value);
+  } catch (err) {
+    console.error('데이터를 불러오는데 실패했어요 😢', err);
+  }
+});
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>Nickname's Log</h1>
+    <p>데이터 콘솔 확인해보세요!</p>
+  </div>
 </template>

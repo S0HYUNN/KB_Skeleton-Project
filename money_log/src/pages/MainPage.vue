07c4926feed5edc582a05621dbf9786e8b52603e
yeni023 <yeeun3011@naver.com>
2025-04-09 17:29:41 +0900

    feat: MainPage 캘린더, 가계부 요약 기능 추가

--- money_log/src/pages/MainPage.vue
+++ money_log/src/pages/MainPage.vue
@@ -1,23 +1,47 @@
 <script setup>
-import DailyMoneyLog from '@/components/DailyMoneyLog';
+import { ref, onMounted } from 'vue';
+import DailyMoneyLog from '@/components/DailyMoneyLog/DailyMoneyLog.vue';
+import CustomCalendar from '@/components/CustomCalendar.vue';
+import MonthlySummary from '@/components/MonthlySummary.vue';
+
+const nickname = ref('');
+
+onMounted(async () => {
+  try {
+    const response = await fetch('http://localhost:3000/user');
+    if (!response.ok) throw new Error('닉네임 로딩 실패');
+    const data = await response.json();
+    nickname.value = data.nickname;
+  } catch (error) {
+    console.error('에러:', error);
+  }
+});
 </script>
 
 <template>
   <div class="main-page">
+    <div class="nickname-title">{{ nickname }}'s Log</div>
     <DailyMoneyLog />
+    <CustomCalendar />
+    <MonthlySummary />
   </div>
 </template>
 
 <style scoped>
 .main-page {
   background-color: #f1f1e8;
-  width: 100%;
-  min-width: 402px;
-  max-width: 402px;
   min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding-top: 48px;
+  padding-top: 56px;
+  gap: 20px;
+}
+
+.nickname-title {
+  font-size: 23px;
+  font-weight: bold;
+  color: #0b570e;
+  margin-bottom: -10px;
 }
 </style>

af8a4d67135ba564e81305de51e5b89a22d456ac
yeni023 <yeeun3011@naver.com>
2025-04-11 01:07:54 +0900

    styles: LogDetail css

--- money_log/src/components/LogDetail/LogDetailFilter.vue
+++ money_log/src/components/LogDetail/LogDetailFilter.vue
@@ -1,25 +1,26 @@
 <template>
-  <div class="filters">
-    <!-- 년도 필터 -->
-    <div class="filter-item-year">
-      <select v-model="selectedYear" @change="updateFilter">
-        <option value="2020">2020</option>
-        <option value="2021">2021</option>
-        <option value="2022">2022</option>
-        <option value="2023">2023</option>
-        <option value="2024">2024</option>
-        <option value="2025">2025</option>
-        <option value="2026">2026</option>
-        <option value="2027">2027</option>
-        <option value="2028">2028</option>
-        <option value="2029">2029</option>
-        <option value="2030">2030</option>
-      </select>
+  <div class="search-bar">
+    <div class="search-wrapper">
+      <img
+        src="@/assets/images/MagnifyingGlass2.svg"
+        class="search-icon"
+        @click="handleSearch"
+      />
+      <input
+        type="text"
+        v-model="searchQuery"
+        placeholder="키워드를 입력하세요"
+        class="search-input"
+        @input="updateFilter"
+        @keydown.enter="handleSearch"
+      />
     </div>
-    <!-- 테마 필터 -->
-    <div class="filter-item-theme">
+  </div>
+  <div class="filter-line">
+    <div class="filter-year">2025</div>
+    <div class="filter-item">
       <select v-model="themeFilter" @change="updateFilter">
-        <option value="allTheme">전체</option>
+        <option value="allTheme">카테고리</option>
         <option value="월급">월급</option>
         <option value="용돈">용돈</option>
         <option value="기타">기타</option>
@@ -30,29 +31,13 @@
         <option value="쇼핑">쇼핑</option>
       </select>
     </div>
-  </div>
-  <div class="filters">
-    <!-- 정렬 기준 설정 -->
-    <div class="sort-btn">
-      <span @click="toggleSortOrder" class="sort-icon">
-        <img
-          :src="sortOrder === 'asc' ? vectorUp : vectorDown"
-          alt="정렬 아이콘"
-          class="sort-icon-img"
-        />
-      </span>
-    </div>
-
-    <!-- 카테고리 필터 -->
     <div class="filter-item">
       <select v-model="selectedOption" @change="updateFilter">
-        <option value="all">전체</option>
+        <option value="all">유형</option>
         <option value="income">수입</option>
         <option value="expense">지출</option>
       </select>
     </div>
-
-    <!-- 기간 필터 -->
     <div class="filter-item">
       <select v-model="selectedPeriod" @change="updateFilter">
         <option value="january">1월</option>
@@ -67,158 +52,146 @@
         <option value="october">10월</option>
         <option value="november">11월</option>
         <option value="december">12월</option>
-        <option value="allmonth">전체</option>
-        <option value="setting">사용자설정</option>
+        <option value="allmonth">월 선택</option>
       </select>
     </div>
-
-    <!-- 검색 필터 -->
-    <div class="filter-item search-wrapper">
-      <input
-        type="text"
-        v-model="searchQuery"
-        placeholder="제목 검색"
-        class="search-input"
-        @input="updateFilter"
-        @keydown.enter="handleSearch"
-      />
+    <span class="sort-icon" @click="toggleSortOrder">
       <img
-        src="@/assets/images/MagnifyingGlass2.svg"
-        class="search-icon"
-        @click="handleSearch"
+        :src="sortOrder === 'asc' ? vectorUp : vectorDown"
+        alt="정렬 아이콘"
+        class="sort-icon-img"
       />
-    </div>
+    </span>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
-import vectorUp from '@/assets/images/vector_up.png';
-import vectorDown from '@/assets/images/vector_down.png';
+import { ref, onMounted } from "vue";
+import vectorUp from "@/assets/images/vector_up.png";
+import vectorDown from "@/assets/images/vector_down.png";
 
-const selectedOption = ref('all');
-const selectedPeriod = ref('allmonth');
-const searchQuery = ref('');
-const selectedYear = ref(''); // 기본값 설정
-const sortOrder = ref('desc'); // 'asc'는 오름차순, 'desc'는 내림차순
-const themeFilter = ref('allTheme'); // 테마 필터
-// filterUpdated 이벤트 정의
+const selectedOption = ref("all");
+const selectedPeriod = ref("allmonth");
+const searchQuery = ref("");
+const selectedYear = ref("");
+const sortOrder = ref("desc");
+const themeFilter = ref("allTheme");
+
 const emit = defineEmits();
 
-// 필터 업데이트 함수
 const updateFilter = () => {
-  emit('filterUpdated', {
+  emit("filterUpdated", {
     category: selectedOption.value,
     period: selectedPeriod.value,
-    year: selectedYear.value,
+    year: "2025",
     searchQuery: searchQuery.value,
     sortOrder: sortOrder.value,
-    theme: themeFilter.value, // 테마 필터 추가
+    theme: themeFilter.value,
   });
 };
 
-// 정렬 방향을 토글하는 함수
 const toggleSortOrder = () => {
-  sortOrder.value = sortOrder.value === 'asc' ? 'desc' : 'asc';
-  updateFilter(); // 정렬 방향 변경 시에도 필터 업데이트
+  sortOrder.value = sortOrder.value === "asc" ? "desc" : "asc";
+  updateFilter();
 };
 
-// 검색 처리 함수
 const handleSearch = () => {
-  updateFilter(); // 검색어가 입력되었을 때 필터 업데이트
+  updateFilter();
 };
+
 onMounted(() => {
-  const currentYear = new Date().getFullYear(); // 현재 년도
-  selectedYear.value = currentYear; // 기본값을 현재 연도로 설정
+  selectedYear.value = "2025";
 });
 </script>
 
 <style scoped>
-.filters {
+.search-bar {
   display: flex;
   align-items: center;
-  justify-content: flex-start;
-}
-
-.sort-btn {
-  color: #888;
-  font-weight: 500;
-  cursor: pointer;
-  margin-right: 0.25rem;
-}
-
-.filter-item {
-  display: flex;
-  align-items: center;
-  border: 1px solid #e0e0e0;
-  border-radius: 5px;
-  background-color: #fff;
-  margin-right: 0.25rem;
-}
-
-.sort-icon {
-  font-size: 14px;
-  color: #4caf50;
-  cursor: pointer;
-}
-
-select,
-.search-input {
-  border: none;
-  background-color: transparent;
-  font-size: 10px; /* 폰트 크기 조정 */
-  color: #333;
-  padding: auto 3px;
-  outline: none;
-}
-
-.search-input {
-  width: 120px;
-  padding-right: 8px; /* 아이콘과의 간격을 설정할 수 있습니다. */
-}
-
-select:focus,
-.search-input:focus {
-  border: none;
-  box-shadow: 0 0 0 2px #4caf50;
-}
-
-.sort-icon-img {
-  width: 14px;
-  height: 10px;
-  object-fit: contain;
-  background-color: white;
+  justify-content: center;
+  margin-bottom: 10px;
 }
 
 .search-wrapper {
   display: flex;
   align-items: center;
-  margin-left: auto; /* 오른쪽으로 밀어내기 */
+  border: 2px solid #ebbb42;
+  border-radius: 20px;
+  padding: 4px 10px;
+  width: 270px;
+  background-color: white;
+}
+
+.search-input {
+  width: 270px;
+  border: 2px solid #ebbb42;
+  border-radius: 20px;
+  padding: 6px 12px;
+  font-size: 11px;
+  line-height: 1.2;
+  outline: none;
 }
 
 .search-icon {
   width: 16px;
   height: 16px;
-  object-fit: contain;
+  margin-left: 8px;
+  margin-right: 5px;
+  cursor: pointer;
 }
 
-.filter-item-year {
-  margin-bottom: 5px;
-  margin-left: 0;
+.search-input {
+  flex: 1;
+  border: none;
+  outline: none;
+  font-size: 11px;
+  background-color: transparent;
 }
 
-.filter-item-year select {
-  font-size: 20px;
-  height: 40px;
-  color: #2e7d32;
-  font-weight: 900;
-}
-.filter-item-theme {
+.filter-line {
   display: flex;
   align-items: center;
-  margin-left: 10.8rem;
-  border: 1px solid #e0e0e0;
-  border-radius: 5px;
-  background-color: #fff;
+  justify-content: space-between;
+  gap: 6px;
+  margin-bottom: 10px;
+}
+
+.filter-year {
+  font-size: 20px;
+  font-weight: bold;
+  color: #2e7d32;
+  margin-top: -2px;
+}
+
+.filter-item {
+  border: 1px solid #dad8d8;
+  border-radius: 0;
+  background-color: white;
+
+  padding: 0;
+  display: flex;
+  align-items: center;
+}
+
+.filter-item select {
+  border: none;
+  background: transparent;
+  outline: none;
+
+  font-size: 10px;
+  color: #0b570e;
+
+  padding: 2px 6px;
+  height: auto;
+  line-height: 1.2;
+}
+
+.sort-icon {
+  cursor: pointer;
+}
+
+.sort-icon-img {
+  width: 18px;
+  height: 17px;
 }
 </style>
